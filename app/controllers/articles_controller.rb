1a8b95f6fb1e3fc0ad8e2d95dd3d9e5d11715115
Priyank Lohariwal <priyank.lohariwal@anchanto.com>
2021-02-05 15:28:24 +0530

    DRY testing in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
+    
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
 
     def create
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully"
             redirect_to article_path(@article)
@@ -22,8 +23,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
         else
@@ -32,13 +32,20 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Article deleted successfully"
         redirect_to articles_path
+    end
+
+    private
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
