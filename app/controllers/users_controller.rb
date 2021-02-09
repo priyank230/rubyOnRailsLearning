63e93e7c8654d6449628c4d6859e9ff32ab2b868
Priyank Lohariwal <priyank.lohariwal@anchanto.com>
2021-02-09 23:47:45 +0530

    Admin functionalities added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show, :edit, :update]
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def index
         @users = User.all
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil if @user == current_user
+        flash[:notice] = "Account and all associated articles successfully deleted"
+        redirect_to articles_path
+    end
+
     private
     def find_user
         @user = User.find(params[:id])
@@ -48,7 +55,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user && !current_user.admin?
             flash[:alert] = "You can edit only your account"
             redirect_to @user
         end
